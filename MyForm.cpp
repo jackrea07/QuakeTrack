0e5da7e2258750d5e1c71dbbd32c7ba2b0ec857a
Jack Rea <87445358+jackrea07@users.noreply.github.com>
2022-04-17 02:01:18 -0400

    Add files via upload

---
+++ MyForm.cpp
@@ -0,0 +1,23 @@
+#include "MyForm.h"
+
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+
+
+
+[STAThread]
+
+
+int main(array<String^>^ args)
+{
+	
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    QuakeUI::MyForm form;
+    Application::Run(% form);
+}
+
