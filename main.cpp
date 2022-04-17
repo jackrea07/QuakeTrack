7821ce5ea4d0e2fa165a7d5e85bfbe507b46f608
Jack Rea <87445358+jackrea07@users.noreply.github.com>
2022-04-16 23:48:56 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ class Quake {
 	string date;
 	float distance;
 	float relevance;
+	float longitude;
+	float latitude;
 	
 public:
 	void EditDistance(float dist) {
@@ -53,12 +55,37 @@ public:
 		distance = 0;
 		relevance = 0;
 	}
-	Quake(float depth, float mag, float dist, float relevance, string date) {
+	Quake(float depth, float mag, float dist, float relevance, string date, float longitude, float latitude) {
 		this->depth = depth;
 		this->mag = mag;
 		this->date = date;
 		this->distance = dist;
 		this->relevance = relevance;
+	}
+	void UpdateDistRel(float userLat, float userLong) {
+		this->distance = float(sqrt(pow(double(double(userLong) - double(longitude)), 2) + pow(double(double(userLat) - double(latitude)), 2))) * 69;
+		int distRel;
+		if (distance < 25)
+			distRel = 10;
+		else if (distance > 25 && distance < 50)
+			distRel = 9;
+		else if (distance > 50 && distance < 100)
+			distRel = 8;
+		else if (distance > 100 && distance < 200)
+			distRel = 7;
+		else if (distance > 200 && distance < 500)
+			distRel = 6;
+		else if (distance > 500 && distance < 1000)
+			distRel = 5;
+		else if (distance > 1000 && distance < 2000)
+			distRel = 4;
+		else if (distance > 2000 && distance < 5000)
+			distRel = 3;
+		else if (distance > 5000 && distance < 10000)
+			distRel = 2;
+		else
+			distRel = 1;
+		this->relevance = distRel * mag;
 	}
 };
 
@@ -124,10 +151,10 @@ void ReadFile(string fileName, vector<Quake>& quakes, float userLong, float user
 
 			getline(stream, date);
 
-			float distance = float(sqrt(pow(double(double(userLong) - double(longitude)), 2) + pow(double(double(userLat) - double(latitude)), 2))) * 69;
+			float distance = float(sqrt(pow(double(double(userLong) - double(longitude)), 2) + pow(double(double(userLat) - double(latitude)), 2))) * 69; //convert to miles from user location
 			float relevance = Relevance(distance, mag);
 
-			Quake quake(depth, mag, distance, relevance, date);
+			Quake quake(depth, mag, distance, relevance, date, longitude, latitude);
 			quakes.push_back(quake);
 		}
 	}
