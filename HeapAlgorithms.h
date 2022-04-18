f10c45dd28affc413fab0acfa1a51da2fca0f0c0
Kevin-Daniel <54856186+Kevin-Daniel@users.noreply.github.com>
2022-04-18 00:38:29 -0400

    Delete HeapAlgorithms.h

--- HeapAlgorithms.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Heap.h"
-class HeapAlgorithms
-{
-public:
-	static vector<Quake> kthSmallest(vector<Quake>& quakes, int k, int metric);
-	static vector<Quake> heapSort(Quake* quakes, int size, string type, int metric);
-};
-
